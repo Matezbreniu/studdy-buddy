c968e83c1495234309a4ecd8919a0e5f514b58e1
Mateusz Bręk <matezbreniu@gmail.com>
2021-11-20 14:32:31 +0100

    UsersListItem styles added

---
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'components/atoms/Button/Button';
+import { Wrapper, StyledAverage, StyledSection, StyledParagraph } from './UsersListItem.styles';
+
+const UsersListItem = ({ userData: { name, average, attendance = '0%' } }) => (
+  <Wrapper>
+    <StyledAverage value={average}>{average}</StyledAverage>
+    <StyledSection>
+      <StyledParagraph bold large>
+        {name}
+      </StyledParagraph>
+      <StyledParagraph>attendance: {attendance}</StyledParagraph>
+    </StyledSection>
+    <Button />
+  </Wrapper>
+);
+
+UsersListItem.propTypes = {
+  userData: PropTypes.shape({
+    attendance: PropTypes.string.isRequired,
+    average: PropTypes.string,
+    name: PropTypes.string.isRequired,
+  }),
+};
+
+export default UsersListItem;
