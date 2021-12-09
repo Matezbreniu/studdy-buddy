de62d62209916589f07e7157fabcbb48d16ebe7c
Mateusz Bręk <matezbreniu@gmail.com>
2021-12-09 11:41:46 +0100

    Added users context

---
+++ src/components/atoms/DeleteButton/DeleteButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ReactComponent as DeleteIcon } from 'assets/icons/delete-icon.svg';
+import { StyledButton } from './DeleteButton.styles';
+
+const DeleteButton = (props) => (
+  <StyledButton {...props}>
+    <DeleteIcon />
+  </StyledButton>
+);
+
+DeleteButton.propTypes = {};
+
+export default DeleteButton;
--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,47 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { users as usersData } from 'data/users';
+import React from 'react';
 import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
-import { Wrapper, StyledList } from './UsersList.styles';
+import PropTypes from 'prop-types';
+import { StyledList } from './UsersList.styles';
+import { Title } from 'components/atoms/Title/Title';
+import { UserShape } from 'types';
 
-const mockAPI = (success) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (usersData) {
-        resolve([...usersData]);
-      } else {
-        reject({ message: 'Error' });
-      }
-    }, 2000);
-  });
-};
-
-const UsersList = (props) => {
-  const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    mockAPI()
-      .then((data) => {
-        setIsLoading(false);
-        setUsers(data);
-      })
-      .catch((err) => console.log(err));
-  }, []);
-
-  const deleteUser = (name) => {
-    const filteredUsers = users.filter((user) => user.name !== name);
-    setUsers(filteredUsers);
-  };
-
+const UsersList = ({ users }) => {
   return (
-    <Wrapper>
-      <h1>{isLoading ? 'Loading...' : 'Users List'}</h1>
+    <>
+      <Title>Students List</Title>
       <StyledList>
         {users.map((userData) => (
-          <UsersListItem deleteUser={deleteUser} key={userData.name} userData={userData} />
+          <UsersListItem key={userData.name} userData={userData} />
         ))}
       </StyledList>
-    </Wrapper>
+    </>
   );
+};
+
+UsersList.propTypes = {
+  deleteUsers: PropTypes.func,
+  users: PropTypes.shape(UserShape),
 };
 export default UsersList;
